0d379aa96ff7e53a5678b5e8da5f78b605c51b84
Brian Maier <18524934+dasfalke@users.noreply.github.com>
2020-02-10 22:04:33 -0800

    End of tutorial

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setCentralWidget(ui->plainTextEdit);
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +14,86 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_actionNew_triggered()
+{
+    currentFilename.clear();                            // clears current, in use, file name
+    ui->plainTextEdit->setPlainText(QString());         // clears the text edit widget
+}
+
+void MainWindow::on_actionOpen_triggered()
+{
+    QString filename = QFileDialog::getOpenFileName(this, "Open file...");
+    QFile file(filename);
+    currentFilename = filename;
+    if (!file.open(QIODevice::ReadOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(filename);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->plainTextEdit->setPlainText(text);
+    file.close();
+}
+
+void MainWindow::on_actionSave_as_triggered()
+{
+    QString filename = QFileDialog::getSaveFileName(this, "Save as...");
+    QFile file(filename);
+    if (!file.open(QFile::WriteOnly | QFile::Text))
+    {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    currentFilename = filename;
+    setWindowTitle(filename);
+    QTextStream out(&file);
+    QString text = ui->plainTextEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void MainWindow::on_actionPrint_triggered()
+{
+    QPrinter printer;
+    printer.setPrinterName("No printer selected");
+    QPrintDialog pDialog(&printer, this);
+    if (pDialog.exec() == QDialog::Rejected)
+    {
+        QMessageBox::warning(this, "Warning", "Cannot access printer");
+        return;
+    }
+    ui->plainTextEdit->print(&printer);
+}
+
+void MainWindow::on_actionExit_triggered()
+{
+    QApplication::quit();
+}
+
+void MainWindow::on_actionCopy_triggered()
+{
+    ui->plainTextEdit->copy();
+}
+//Good bye  ui->plainTextEdit->cut();
+void MainWindow::on_actionCut_triggered()
+{
+    ui->plainTextEdit->cut();
+}
+
+void MainWindow::on_actionPaste_triggered()
+{
+    ui->plainTextEdit->paste();
+}
+
+void MainWindow::on_actionUndo_triggered()
+{
+    ui->plainTextEdit->undo();
+}
+
+void MainWindow::on_actionRedo_triggered()
+{
+    ui->plainTextEdit->redo();
+}
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFile>
+#include <QFileDialog>
+#include <QTextStream>
+#include <QMessageBox>
+#include <QtPrintSupport/QPrinter>      // Differs from tutorial: "#include <QPrinter>
+#include <QtPrintSupport/QPrintDialog>  // Differs from tutorial: "#include <QPrintDialog>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -15,7 +21,30 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_as_triggered();
+
+    void on_actionPrint_triggered();
+
+    void on_actionExit_triggered();
+
+    void on_actionCopy_triggered();
+
+    void on_actionCut_triggered();
+
+    void on_actionPaste_triggered();
+
+    void on_actionUndo_triggered();
+
+    void on_actionRedo_triggered();
+
 private:
     Ui::MainWindow *ui;
+    QString currentFilename = "";
 };
+
 #endif // MAINWINDOW_H
